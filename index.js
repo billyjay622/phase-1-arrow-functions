581f34b8e5674ffce063079902741a09ca77260d
William Barr <williamjaybarr@gmail.com>
2022-07-18 23:00:31 -0400

    commit tree 26d98501c665bac87d36a2abe30520a31d9e8d81

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide() {}
+
+let a = 2000
+let b = 100
+function divide(a, b) {
+    return a/b
+}
+
+const square = parameter => parameter ** parameter;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
